4eb9e196ca41c528950cf5fe8b22dc5bcd38d710
latino-deluxe <emanuelelatino2206@gmail.com>
2019-01-31 10:43:51 +0100

    inizio comunicazione BT

--- src/chat.cpp
+++ src/chat.cpp
@@ -4,5 +4,23 @@
 
 void teamZone() {
   calculateZoneIndex();
+  iAmHere = zoneIndex;
+  if (count < 30 ) {
+    count++;
+  }
+  else {
+    count = 0;
+    BT.write(iAmHere);
+  }
+}
 
+void whereAreYou() {
+  calculateZoneIndex();
+  if (BT.available() > 0) {
+    friendZone = BT.read();
+  }
+  if (zoneIndex == friendZone) {
+    if (ruolo == HIGH) goalie();
+    else centroporta();
+  }
 }
---
+++ src/chat.h
@@ -0,0 +1,2 @@
+void whereAreYou();
+void teamZone();
