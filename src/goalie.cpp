ab41a2ee82c2ded7df83f8bd6d6f10135cf2b8c5
latino-deluxe <emanuelelatino2206@gmail.com>
2019-02-26 15:00:39 +0100

    Goalie da testare (credo di aver scritto tutto)

--- src/goalie.cpp
+++ src/goalie.cpp
@@ -8,86 +8,37 @@
 #include "vars.h"
 
 void goalie() {
-  // attack direction based on the sensor that sees the ball
   int atk_direction = 0;
-  // attack direction based on the sensor that sees the ball
   int atk_speed = 0;
   // directions going around the ball
   int goaliedirection[20] = {AA0,  AA1,  AA2,  AA3,  AA4,  AA5,  AA6,
                              AA7,  AA8,  AA9,  AA10, AA11, AA12, AA13,
                              AA14, AA15, AA16, AA17, AA18, AA19};
 
-  // PALLA DI FRONTE
-  /*
-  if (ball_sensor == 0)
-  {
-    if(ball_distance>=2) atk_speed = 180;
-    atk_speed = GOALIE_MAX;
+  atk_speed = 180;                              //forse inutile, lo imposto per livellare
+  atk_direction = goaliedirection[ball_sensor]; // going around the ball (inseguo la palla)
+
+  //PALLA DIETRO
+  if(ball_sensor==9 || ball_sensor==10 || ball_sensor==1) {
+    if(ball_distance < 4) { //se la palla è vicina decido come muovermi in base alla zona per non uscire
+      atk_speed = 200;
+      if(zoneIndex!=8 || zoneIndex!=5 || zoneIndex!=2) atk_direction = 135;
+      if(zoneIndex!=0 || zoneIndex!=3 || zoneIndex!=9) atk_direction = 225;
+    }
+  else {    //se la palla è lontana mi avvicino più velocemente con angolo più stretto
+      atk_speed = 255;
+      if(zoneIndex!=8 || zoneIndex!=5 || zoneIndex!=2) atk_direction = 200;
+      if(zoneIndex!=0 || zoneIndex!=3 || zoneIndex!=9) atk_direction = 160;
+    }
   }
-  else
-  {
-    if(ball_distance>=2) atk_speed = 180;
-    atk_speed = GOALIE_MIN;
+
+  //STORCIMENTO (senza camera, alla vecchia maniera)
+  if(ball_sensor==19 || ball_sensor==0 || ball_sensor==1) {
+    atk_speed = 255;
+    if(zoneIndex==0) atk_direction = 45;
+    if(zoneIndex==1) atk_direction = 0;
+    if(zoneIndex==2) atk_direction = 315;
   }
-  */
-  atk_speed = 180;
-  atk_direction = goaliedirection[ball_sensor]; // going around the ball
-
-  /*
-
-    // controllo per ridurre oscillazione quando ha la palla in avanti
-    if (ball_sensor == 1) {
-      if (ball_distance <= 2) {   //se la palla è vicina
-        atk_speed = GOALIE_MAX;
-        atk_direction = 15;
-      }
-    }
-
-
-    if (ball_sensor == 19) {
-      if (ball_distance <= 2) {   //se la palla è vicina
-        atk_speed = GOALIE_MAX;
-        atk_direction = 345;
-      }
-    }
-
-    if ((ball_sensor == 10) || (ball_sensor == 9) || (ball_sensor == 11))  // se
-    la palla é dietro e vicina la scanso a 90 a dx o sx dove ho piú spazio
-    {
-      if (ball_distance < 4)
-      {
-          if (us_sx>=80)
-          {
-            atk_direction = 225;
-          }
-          else
-          {
-            atk_direction = 135;
-          }
-       }
-      else //la palla é lontana anche se dietro le vado quasi addosso in
-    velocita
-       {
-
-        if(us_sx>=80) {
-           atk_direction = 200;
-           atk_speed = GOALIE_MAX;
-         }
-         else {
-           atk_direction = 160;
-           atk_speed = GOALIE_MAX;
-         }
-
-      }
-    }
-    /*if (ball_distance>=3){
-        if ((status_x==OVEST) && (status_y==NORD)){
-          atk_speed=200;
-        }
-        else if ((status_x==EST) && (status_y==NORD)) {
-          atk_speed=200;
-        }
-    }*/
 
   preparePID(atk_direction, atk_speed);
 }
