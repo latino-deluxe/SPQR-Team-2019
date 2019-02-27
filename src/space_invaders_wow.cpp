a646b01ca6ceb654fe71b5999918503a198e6ebe
EmaMaker <emamaker0@gmail.com>
2019-02-27 16:14:06 +0100

    space_invaders_3 carino :D

--- src/space_invaders_wow.cpp
+++ src/space_invaders_wow.cpp
@@ -19,29 +19,48 @@ void keeper() {
 }
 
 void space_invaders_3() {
-  int dir = 0, vel = 185;
+  int dir = 0, vel = 215;
 
   if (zoneIndex >= 6) {
     if (ball_sensor >= 1 && ball_sensor <= 6) {
-      // if it's not in the right zone (8), go right
-      if (zoneIndex != 8) {
-        dir = 90;
+      if (zoneIndex < 8) {
+        preparePID(90, vel);
+      } else {
+        preparePID(0, 0);
       }
-    } else if (ball_sensor >= 14 && ball_sensor >= 19) {
-      // if it's not in the left zone (6), go left
-      if (zoneIndex != 6) {
-        dir = 270;
+    } else if (ball_sensor >= 14 && ball_sensor <= 19) {
+      if (zoneIndex > 6) {
+        preparePID(270, vel);
+      } else {
+        preparePID(0, 0);
       }
-    } else if (ball_sensor > 6 && ball_sensor < 14) {
-      goalie(); // ball behind: goalie
     } else {
-      vel = 0;
-      dir = 0; // ball in front of the robot: stop
+      preparePID(0, 0);
     }
-    preparePID(dir, vel);
   } else {
     centerGoalPost();
   }
+  // if (ball_sensor >= 1 && ball_sensor <= 6) {
+  //   // if it's not in the right zone (8), go right
+  //   if (zoneIndex != 8) {
+  //     dir = 90;
+  //   } else {
+  //     vel = 0;
+  //   }
+  // } else if (ball_sensor >= 14 && ball_sensor <= 19) {
+  //   // if it's not in the left zone (6), go left
+  //   if (zoneIndex != 6) {
+  //     dir = 270;
+  //   } else {
+  //     vel = 0;
+  //   }
+  // } else if (ball_sensor > 6 && ball_sensor < 14) {
+  //   goalie(); // ball behind: goalie
+  // } else {
+  //   vel = 0;
+  //   dir = 0; // ball in front of the robot: stop
+  // }
+  // preparePID(dir, vel);
 }
 
 void space_invaders() {
