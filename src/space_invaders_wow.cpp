33bfc2d9bb1d750e50be4dc09efb6b0cf7e16ad6
EmaMaker <emamaker0@gmail.com>
2019-03-11 18:56:34 +0100

    portierino carino :D

--- src/space_invaders_wow.cpp
+++ src/space_invaders_wow.cpp
@@ -26,164 +26,40 @@ byte vel_por = 255;
 long diff;
 float ball_distance_precise;
 
-void space_invaders_m() {
-
-  if (i = 0) {
-    ball_array[0] = ball_distance;
-    i++;
-  } else if (i = 1) {
-    ball_array[1] = ball_distance;
-    i++;
-  }
-  if (i = 2) {
-    ball_array[2] = ball_distance;
-    i++;
-  } else if (i = 3) {
-    ball_array[3] = ball_distance;
-    i++;
-  } else if (i = 4) {
-    ball_array[4] = ball_distance;
-    i = 0;
-  }
-
-  comunicazione(1000);
-
-  ball_distance_precise = (ball_array[0] + ball_array[1] + ball_array[2] +
-                           ball_array[3] + ball_array[4]) /
-                          5;
-  ball_distance_precise = (ball_distance_precise * 100) / 100;
-
-  if (comrade == true) {
-    if (ball_sensor == 0) {
-      brake();
-      readIMU(); // reads the imu euler angle >imu_current_euler<
-      recenter(1.0);
-    }
-    if ((ball_sensor >= 1) && (ball_sensor <= 6)) {
-      drivePID(90, GOALIE_P);
-    }
-    if ((ball_sensor <= 19) && (ball_sensor >= 14)) {
-      drivePID(270, GOALIE_P);
-    }
-    if ((ball_sensor < 14) && (ball_sensor > 6)) {
-      // menamoli();
-      goalie();
-    }
-
-    if (us_px > 35) {
-      drivePID(180, 150);
-    }
-    if (us_px < 18) {
-      drivePID(0, 150);
-    }
-    atk_p = false;
-  }
-
-  else {
-    // if (ball_distance_precise < 3) {
-    if ((((bd - ball_distance_precise) != 0) ||
-         ((bd - ball_distance_precise) != -0.2) ||
-         ((bd - ball_distance_precise) != 0.2))) {
-      bd = ball_distance_precise;
-    } else
-      ta = millis();
-
-    diff = millis() - ta;
-    if ((diff > 4000) && (diff != millis())) {
-      atk_p = true;
-    }
-    //}
-
-    if (atk_p == false) {
-
-      if (ball_sensor == 0) {
-        brake();
-        readIMU(); // reads the imu euler angle >imu_current_euler<
-        recenter(1.0);
-      }
-      if ((ball_sensor >= 1) && (ball_sensor <= 6)) {
-        drivePID(90, GOALIE_P);
-      }
-      if ((ball_sensor <= 19) && (ball_sensor >= 14)) {
-        drivePID(270, GOALIE_P);
-      }
-      if ((ball_sensor < 14) && (ball_sensor > 6)) {
-        //  menamoli();
-        goalie();
-      }
-
-      if (us_px > 35) {
-        drivePID(180, 150);
-      }
-      if (us_px < 18) {
-        drivePID(0, 150);
-      }
-    }
-
-    else if ((atk_p == true) && (ball_distance_precise < 0.7)) {
-      // menamoli();
-      goalie();
-    } else if ((atk_p == true) && (ball_distance_precise >= 0.7)) {
-
-      if (ball_sensor == 0) {
-        brake();
-        readIMU(); // reads the imu euler angle >imu_current_euler<
-        recenter(1.0);
-      }
-      if ((ball_sensor >= 1) && (ball_sensor <= 6)) {
-        drivePID(90, GOALIE_P);
-      }
-      if ((ball_sensor <= 19) && (ball_sensor >= 14)) {
-        drivePID(270, GOALIE_P);
-      }
-      if ((ball_sensor < 14) && (ball_sensor > 6)) {
-        // menamoli();
-        goalie();
-      }
-
-      if (us_px > 37) {
-        drivePID(180, 150);
-      }
-      if (us_px < 18) {
-        drivePID(0, 150);
-      }
-    }
-  }
-
-  return;
-}
-
 void space_invaders_3() {
-  if (us_px > 50) {
-    centerGoalPost();
-  } else {
-    if (ball_sensor >= 1 && ball_sensor <= 6) {
-      if (us_dx > 60)
-        preparePID(90, 180);
-      else
-        preparePID(0, 0);
-    } else if (ball_sensor >= 14 && ball_sensor <= 19) {
-      if (us_sx > 60)
-        preparePID(270, 180);
-      else
-        preparePID(0, 0);
-    } else if (ball_sensor < 14 && ball_sensor > 6) {
-      goalie();
-      // preparePID(180, 180);
-    } else {
+  if (ball_sensor >= 1 && ball_sensor <= 6) {
+    if (us_dx > 60 || us_sx < 60)
+      preparePID(90, 180);
+    else
       preparePID(0, 0);
-    }
+  } else if (ball_sensor >= 14 && ball_sensor <= 19) {
+    if (us_sx > 60 || us_dx < 60)
+      preparePID(270, 180);
+    else
+      preparePID(0, 0);
+  } else if (ball_sensor < 14 && ball_sensor > 6) {
+    goalie();
+    // preparePID(180, 180);
+  } else {
+    // if (us_px > 50) {
+    //   centerGoalPost();
+    // } else if (us_px < 40) {
+    //   preparePID(0, 100);
+    // } else {
+    preparePID(0, 0);
+    // }
   }
 }
 
 void centerGoalPost() {
-  int vel = 200;
-  if ((zoneIndex >= 0 && zoneIndex <= 2) || zoneIndex == 4)
+  int vel = 150;
+  if ((zoneIndex >= 0 && zoneIndex <= 2) || zoneIndex == 4) {
     preparePID(180, vel);
-  else if (zoneIndex == 3 || zoneIndex == 6)
+  } else if (zoneIndex == 3 || zoneIndex == 6) {
     preparePID(90, vel);
-  else if (zoneIndex == 5 || zoneIndex == 8)
+  } else if (zoneIndex == 5 || zoneIndex == 8) {
     preparePID(270, vel);
-  else
+  } else {
     preparePID(0, 0);
+  }
 }
